560e1a424ff3838f58e23c9b8bfbee9c8cc7e205
tuyalts <tuyana21lombo@gmail.com>
2025-09-26 21:33:58 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
+               <a href="/author">author</a>
            </body>
-        </html>""" 
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Ломбоцыренова Туяна Владимировна"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
