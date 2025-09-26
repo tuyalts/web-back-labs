f86c61681fe54dc59e7291366a270f2209506e39
tuyalts <tuyana21lombo@gmail.com>
2025-09-26 23:36:37 +0700

    Улучшение страницы для ошибки 404

--- app.py
+++ app.py
@@ -89,7 +89,31 @@ def created():
 
 @app.errorhandler(404)
 def not_found(err):
-    return "нет такой страницы", 404
+    return ''' 
+<!doctype html>
+<html>
+    <head>
+        <style>
+            body {
+                text-align: center;
+            }
+            h1 {
+                color: #red;
+                font-size: 48px;
+            }
+            img {
+                max-width: 300px;
+                margin: 20px 0;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>404</h1>
+        <h2>Такой страницы не существует</h2>
+        <img src="''' + url_for('static', filename='okak.jpg') + '''" alt="Ошибка 404">
+    </body>
+</html>    
+''', 404
 
 @app.route("/clearcounter")
 def clearcounter():
