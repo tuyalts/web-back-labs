583d8e7e68c0d00c07342844fa0cac3d46dfefcc
tuyalts <tuyana21lombo@gmail.com>
2025-09-26 21:57:29 +0700

    Служебная информация

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
+import datetime
 app = Flask(__name__)
 
 @app.route("/web")
@@ -46,11 +47,19 @@ count = 0
 def counter():
     global count
     count +=1
+    time = datetime.datetime.today()
+    url = request.url
+    client_ip = request.remote_addr
+
     return '''
 <!doctype html>
 <html>
     <body>
         Сколько раз вы сюда заходили: ''' + str(count) + '''
+        <hr>
+        Дата и время: ''' + str(time) + '''<br>
+        Запрошенный адрес: ''' + url + '''<br>
+        Ваш IP-адрес: ''' + client_ip + '''<br>
     </body>
 </html>
 '''
