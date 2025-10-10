7db36fcb9be93acce9dd4f18b658658e8cbe8cda
tuyalts <tuyana21lombo@gmail.com>
2025-10-10 20:39:32 +0700

    Имя студента

--- app.py
+++ app.py
@@ -297,4 +297,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Цыренова  Яна'
+    return render_template('example.html', name=name)
