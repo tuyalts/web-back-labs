9a2fbf5921052ea02aabd1090574700f64107e0f
tuyalts <tuyana21lombo@gmail.com>
2025-10-24 18:21:43 +0700

    Рефакторинг: выделение первой лабораторной

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
-app = Flask(__name__)
+from lab1 import lab1
 
-@app.route("/lab1/web")
-def web():
+app = Flask(__name__)
+app.register_blueprint(lab1)
+
+@app.route("/")
+def start():
     return """<!doctype html>
         <html>
            <body>
@@ -14,82 +17,6 @@ def web():
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=uft-8'
         }
-
-@app.route("/lab1/author")
-def author():
-    name = "Ломбоцыренова Туяна Владимировна"
-    group = "ФБИ-33"
-    faculty = "ФБ"
-
-    return """<!doctype html>
-        <html>
-            <body>
-                <p>Студент: """ + name + """</p>
-                <p>Группа: """ + group + """</p>
-                <p>Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
-            </body>
-        </html>"""
-
-@app.route('/lab1/image')
-def image():
-    path1 = url_for("static", filename="lab1.css")
-    path = url_for("static", filename="oak.jpg")
-    return '''
-<!doctype html>
-<html>
-    <head>
-        <link rel="stylesheet" href="''' + path1 + '''">
-    </head>
-    <body>
-        <h1>Дуб</h1>
-        <img src="''' + path + '''">
-    </body>
-</html>
-''', 200, {
-    'Content-Language': 'ru-RU',
-    'X-Server': 'sample',
-    'X-Student-Name': 'Ломбоцыренова Туяна'
-}
-
-count = 0
-
-@app.route('/lab1/counter')
-def counter():
-    global count
-    count +=1
-    time = datetime.datetime.today()
-    url = request.url
-    client_ip = request.remote_addr
-
-    return '''
-<!doctype html>
-<html>
-    <body>
-        Сколько раз вы сюда заходили: ''' + str(count) + '''
-        <hr>
-        Дата и время: ''' + str(time) + '''<br>
-        Запрошенный адрес: ''' + url + '''<br>
-        Ваш IP-адрес: ''' + client_ip + '''<br>
-        <a href="''' + url_for('clearcounter') + '''">Очистить счетчик</a>
-    </body>
-</html>
-'''
-
-@app.route("/lab1/info")
-def info():
-    return redirect("/lab1/author")
-
-@app.route("/lab1/created")
-def created():
-    return '''
-<!doctype html>
-    <body>
-        <h1>Создано успешно</h1>
-        <div><i>что-то создано...</i></div>
-    </body>
-</html>
-''', 201
 
 @app.errorhandler(404)
 def not_found(err):
@@ -165,105 +92,6 @@ def index():
     </body>
 </html>
 '''
-
-@app.route("/lab1")
-def lab1():
-    return '''
-<!doctype html>
-<html>
-    <head>
-        <title>Лабораторная 1</title>
-    </head>
-    <body>
-        <p>
-            Flask — фреймворк для создания веб-приложений на языке
-            программирования Python, использующий набор инструментов
-            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
-            называемых микрофреймворков — минималистичных каркасов
-            веб-приложений, сознательно предоставляющих лишь самые ба-
-            зовые возможности.
-        </p>
-        <a href="''' + url_for('index') + '''">На главную</a>
-    </body>
-</html>
-'''
-
-@app.route('/lab1/400')
-def error_400():
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h1>400 Bad Request</h1>
-        <p>Сервер не может обработать запрос из-за синтаксической ошибки.</p>
-    </body>
-</html>
-''', 400
-
-@app.route('/lab1/401')
-def error_401():
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h1>401 Unauthorized</h1>
-        <p>Для доступа к запрашиваемому ресурсу требуется аутентификация.</p>
-    </body>
-</html>
-''', 401
-
-@app.route('/lab1/402')
-def error_402():
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h1>402 Payment Required</h1>
-        <p>Запрос не может быть обработан до осуществления оплаты.</p>
-    </body>
-</html>
-''', 402
-
-@app.route('/lab1/403')
-def error_403():
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h1>403 Forbidden</h1>
-        <p>Доступ к запрашиваемому ресурсу запрещен.</p>
-    </body>
-</html>
-''', 403
-
-@app.route('/lab1/405')
-def error_405():
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h1>405 Method Not Allowed</h1>
-        <p>Использованный метод HTTP не поддерживается для данного ресурса.</p>
-    </body>
-</html>
-''', 405
-
-@app.route('/lab1/418')
-def error_418():
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h1>418 I'm a teapot</h1>
-        <p>Я чайник. Не могу заваривать кофе.</p>
-    </body>
-</html>
-''', 418
-
-@app.route('/lab1/500')
-def err_500():
-    res = 1 / 0
-    return "лол"
 
 @app.route('/lab2/a')
 def a():
---
+++ lab1.py
@@ -0,0 +1,205 @@
+from flask import Blueprint, url_for, redirect
+lab1 = Blueprint('lab1', __name__)
+
+
+@lab1.route("/lab1/web")
+def web():
+    return """<!doctype html>
+        <html>
+           <body>
+               <h1>web-сервер на flask</h1>
+               <a href="/author">author</a>
+           </body>
+        </html>""", 200, {
+            'X-Server': 'sample',
+            'Content-Type': 'text/plain; charset=uft-8'
+        }
+
+
+@lab1.route("/lab1/author")
+def author():
+    name = "Ломбоцыренова Туяна Владимировна"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
+
+
+@lab1.route('/lab1/image')
+def image():
+    path1 = url_for("static", filename="lab1.css")
+    path = url_for("static", filename="oak.jpg")
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="''' + path1 + '''">
+    </head>
+    <body>
+        <h1>Дуб</h1>
+        <img src="''' + path + '''">
+    </body>
+</html>
+''', 200, {
+    'Content-Language': 'ru-RU',
+    'X-Server': 'sample',
+    'X-Student-Name': 'Ломбоцыренова Туяна'
+}
+
+
+count = 0
+
+
+@lab1.route('/lab1/counter')
+def counter():
+    global count
+    count +=1
+    time = datetime.datetime.today()
+    url = request.url
+    client_ip = request.remote_addr
+
+    return '''
+<!doctype html>
+<html>
+    <body>
+        Сколько раз вы сюда заходили: ''' + str(count) + '''
+        <hr>
+        Дата и время: ''' + str(time) + '''<br>
+        Запрошенный адрес: ''' + url + '''<br>
+        Ваш IP-адрес: ''' + client_ip + '''<br>
+        <a href="''' + url_for('clearcounter') + '''">Очистить счетчик</a>
+    </body>
+</html>
+'''
+
+
+@lab1.route("/lab1/info")
+def info():
+    return redirect("/lab1/author")
+
+
+@lab1.route("/lab1/created")
+def created():
+    return '''
+<!doctype html>
+    <body>
+        <h1>Создано успешно</h1>
+        <div><i>что-то создано...</i></div>
+    </body>
+</html>
+''', 201
+
+
+@lab1.route("/lab1")
+def lab():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>Лабораторная 1</title>
+    </head>
+    <body>
+        <p>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые ба-
+            зовые возможности.
+        </p>
+        <a href="''' + url_for('index') + '''">На главную</a>
+    </body>
+</html>
+'''
+
+
+@lab1.route('/lab1/400')
+def error_400():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>400 Bad Request</h1>
+        <p>Сервер не может обработать запрос из-за синтаксической ошибки.</p>
+    </body>
+</html>
+''', 400
+
+
+@lab1.route('/lab1/401')
+def error_401():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>401 Unauthorized</h1>
+        <p>Для доступа к запрашиваемому ресурсу требуется аутентификация.</p>
+    </body>
+</html>
+''', 401
+
+
+@lab1.route('/lab1/402')
+def error_402():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>402 Payment Required</h1>
+        <p>Запрос не может быть обработан до осуществления оплаты.</p>
+    </body>
+</html>
+''', 402
+
+
+@lab1.route('/lab1/403')
+def error_403():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>403 Forbidden</h1>
+        <p>Доступ к запрашиваемому ресурсу запрещен.</p>
+    </body>
+</html>
+''', 403
+
+
+@lab1.route('/lab1/405')
+def error_405():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>405 Method Not Allowed</h1>
+        <p>Использованный метод HTTP не поддерживается для данного ресурса.</p>
+    </body>
+</html>
+''', 405
+
+
+@lab1.route('/lab1/418')
+def error_418():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>418 I'm a teapot</h1>
+        <p>Я чайник. Не могу заваривать кофе.</p>
+    </body>
+</html>
+''', 418
+
+
+@lab1.route('/lab1/500')
+def err_500():
+    res = 1 / 0
+    return "лол"
