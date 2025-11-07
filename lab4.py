3af8f319b5afcade3d9fd6178a066656a3fdb7c3
tuyalts <tuyana21lombo@gmail.com>
2025-11-07 20:49:49 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -121,6 +121,13 @@ def tree():
     return redirect('/lab4/tree')
 
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'tuyalts', 'password': '2102'},
+    {'login': 'ivan', 'password': '0909'},
+]
+
 @lab4.route('/lab4/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -129,10 +136,13 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('/lab4/login.html', 
-                                login=login, authorized=True)
-    
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', 
+                                    login=login, authorized=True)
+        
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error=error, 
                             authorized=False)
+
+
