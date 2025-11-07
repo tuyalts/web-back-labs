f7b93b06b5dc9c7fa9cc7db178fcceb6d4a46761
tuyalts <tuyana21lombo@gmail.com>
2025-11-07 19:09:50 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -24,3 +24,77 @@ def div():
         return render_template('lab4/div.html', error='На ноль делить нельзя')
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('/lab4/sum-form.html')
+
+
+@lab4.route('/lab4/sum', methods = ['POST'])
+def sum():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '':
+        x1 = '0'
+    if x2 == '':
+        x2 = '0'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/mult-form')
+def mult_form():
+    return render_template('/lab4/mult-form.html')
+
+
+@lab4.route('/lab4/mult', methods = ['POST'])
+def mult():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '':
+        x1 = '1'
+    if x2 == '':
+        x2 = '1'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 * x2
+    return render_template('lab4/mult.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('/lab4/sub-form.html')
+
+
+@lab4.route('/lab4/sub', methods = ['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/exp-form')
+def exp_form():
+    return render_template('/lab4/exp-form.html')
+
+
+@lab4.route('/lab4/exp', methods = ['POST'])
+def exp():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/exp.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 ** x2
+    return render_template('lab4/exp.html', x1=x1, x2=x2, result=result)
