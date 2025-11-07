ea534bfcbec51c4c12123d6c9b7ac5204e6115ab
tuyalts <tuyana21lombo@gmail.com>
2025-11-07 17:16:16 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import datetime
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 
 @app.route("/")
@@ -28,6 +30,7 @@ def start():
                 <li><a href="/lab1">Первая лабораторная</a></li>
                 <li><a href="/lab2">Вторая лабораторная</a></li>
                 <li><a href="/lab3">Третья лабораторная</a></li>
+                <li><a href="/lab4">Четвертая лабораторная</a></li>
             </ul>
         </div>
         
