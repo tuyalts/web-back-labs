4dbe38d9a324f663cde394ce97c36435b469deb2
tuyalts <tuyana21lombo@gmail.com>
2025-11-07 18:29:26 +0700

    Деление

--- app.py
+++ app.py
@@ -71,35 +71,6 @@ def not_found(err):
 ''', 404
 
 
-@app.errorhandler(404)
-def not_found(err):
-    return ''' 
-<!doctype html>
-<html>
-    <head>
-        <style>
-            body {
-                text-align: center;
-            }
-            h1 {
-                color: #red;
-                font-size: 48px;
-            }
-            img {
-                max-width: 300px;
-                margin: 20px 0;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>404</h1>
-        <h2>Такой страницы не существует</h2>
-        <img src="''' + url_for('static', filename='okak.jpg') + '''" alt="Ошибка 404">
-    </body>
-</html>    
-''', 404
-
-
 @app.route("/index")
 def index():
     return '''
