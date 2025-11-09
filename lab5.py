ff1fefe73b976bc3e2ae4c5ae04d983cf99da02a
tuyalts <tuyana21lombo@gmail.com>
2025-11-10 00:59:52 +0700

    заготовка к 5-ой лабораторной

---
+++ lab5.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template, request, redirect, session
+lab5 = Blueprint('lab5', __name__)
+
+
+@lab5.route('/lab5')
+def lab():
+    return render_template('lab5/lab5.html')
+
+
+# @lab5.route('/lab5/login')
+# def login():
+
+
+# @lab5.route('/lab5/register')
+# def register():
+
+
+# @lab5.route('/lab5/create')
+# def create():
+
+
+# name = 'anonymous'
