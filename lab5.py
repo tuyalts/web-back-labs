30e5cb44939f1cb2e0d104a058f7e2b40cac41d9
tuyalts <tuyana21lombo@gmail.com>
2026-01-04 01:30:27 +0700

    Аунтентификация

--- lab5.py
+++ lab5.py
@@ -1,15 +1,59 @@
 from flask import Blueprint, render_template, request, redirect, session
 import psycopg2
+from psycopg2.extras import RealDictCursor
+
 lab5 = Blueprint('lab5', __name__)
 
 
 @lab5.route('/lab5')
 def lab():
-    return render_template('lab5/lab5.html')
+    return render_template('lab5/lab5.html', login=session.get('login'))
 
 
-# @lab5.route('/lab5/login')
-# def login():
+@lab5.route('/lab5/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login and password):
+        return render_template('lab5/login.html', error="Заполните поля")    
+
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database =  'tuyana_lombocyrenova_knowledge_base',
+        user = 'tuyana_lombocyrenova_knowledge_base',
+        password = '799034'
+    )
+
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+
+    cur.execute(f"SELECT * FROM  users WHERE login='{login}';")
+    user = cur.fetchone()
+
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль не верны')
+
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль не верны')
+
+    session['login'] = login
+    cur.close()
+    conn.close()
+    return render_template('lab5/success_login.html', login=login)
+
+
+@lab5.route('/lab5/logout')
+def logout():
+    session.pop('user', None)
+    session.pop('login', None)
+    return redirect('/lab5')
 
 
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
@@ -20,7 +64,7 @@ def register():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if not (login or password):
+    if not (login and password):
         return render_template('lab5/register.html', error='Заполните все поля')
     
     conn = psycopg2.connect(
