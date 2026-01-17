875c59318c33c25fc470860c9862210384f7fa0f
tuyalts <tuyana21lombo@gmail.com>
2026-01-18 06:21:40 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -52,7 +52,37 @@ def api():
                     'result': 'success',
                     'id': id
                 }
+        
+    if data['method'] == 'cancelation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc':'2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office is not booked'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc':'2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'You are not the tenant'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
 
+                
     return {
         'jsonrpc':'2.0',
             'error': {
@@ -61,4 +91,4 @@ def api():
             },
             'id': id
     }
-        
+
