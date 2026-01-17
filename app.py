37353929e08fd7b2e03dce84713f964edd8c6e96
tuyalts <tuyana21lombo@gmail.com>
2026-01-18 03:28:11 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 load_dotenv()
 
@@ -21,6 +22,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 
 @app.route("/")
@@ -42,6 +44,8 @@ def start():
                 <li><a href="/lab2">Вторая лабораторная</a></li>
                 <li><a href="/lab3">Третья лабораторная</a></li>
                 <li><a href="/lab4">Четвертая лабораторная</a></li>
+                <li><a href="/lab5">Пятая лабораторная</a></li>
+                <li><a href="/lab6">Шестая лабораторная</a></li>
             </ul>
         </div>
         
