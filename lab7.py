83250c3835d8106824670e30afd92fae3db9c1d7
tuyalts <tuyana21lombo@gmail.com>
2026-01-18 23:35:51 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -49,3 +49,11 @@ def del_film(id):
     return '', 204
 
 
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        return {"error": "Фильм не найден"}, 404
+    film = request.get_json()
+    films[id] = film
+    return films[id]
+
