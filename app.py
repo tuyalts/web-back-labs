0cc6aefa3fd05e82f30e659c5ba0d6cbacbdc10d
tuyalts <tuyana21lombo@gmail.com>
2026-01-18 20:29:47 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 
 load_dotenv()
 
@@ -23,6 +24,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 
 @app.route("/")
