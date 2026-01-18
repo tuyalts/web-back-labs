85a84c57e4db070287d644e7d96d00293723c178
tuyalts <tuyana21lombo@gmail.com>
2026-01-19 02:21:19 +0700

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -21,6 +21,9 @@ function fillFilmList() {
 
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
+            editButton.onclick = function() {
+                editFilm(i);
+            }
             
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
@@ -48,22 +51,19 @@ function deleteFilm(id, title) {
     fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
     .then(function () {
         fillFilmList();
-        });
+    });
 }
 
 function showModal() {
     document.querySelector('div.modal').style.display = 'block';
 }
+
 function hideModal() {
     document.querySelector('div.modal').style.display = 'none';
 }
 
 function cancel() {
     hideModal();
-}
-
-function addFilm() {
-    showModal();
 }
 
 function addFilm() {
@@ -75,16 +75,32 @@ function addFilm() {
     showModal();
 }
 
+function editFilm(id) {
+    fetch(`/lab7/rest-api/films/${id}`)
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (film) {
+        document.getElementById('id').value = id;
+        document.getElementById('title').value = film.title;
+        document.getElementById('title-ru').value = film.title_ru;
+        document.getElementById('year').value = film.year;
+        document.getElementById('description').value = film.description;
+        showModal();
+    });
+}
+
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title-ru').value,
         year: document.getElementById('year').value,
         description: document.getElementById('description').value,
-    }
+    };
 
-    const url = `/lab7/rest-api/films`;
-    const method = 'POST';
+    const url = id === '' ? '/lab7/rest-api/films/' : `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST' : 'PUT';
 
     fetch(url, {
         method: method,
@@ -96,6 +112,7 @@ function sendFilm() {
         hideModal();
     });
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     fillFilmList();
 });
