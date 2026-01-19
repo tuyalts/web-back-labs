ab3fe2c5dfd80a74ad43ef3a8669b21b8c79b628
tuyalts <tuyana21lombo@gmail.com>
2026-01-19 09:33:54 +0700

    Заготовка к 9-ой лабораторной

---
+++ lab9.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template, request, redirect, session, flash
+
+lab9 = Blueprint('lab9', __name__)
+
+@lab9.route('/lab9/')
+def main():
+    return render_template('lab9/index.html')
+
